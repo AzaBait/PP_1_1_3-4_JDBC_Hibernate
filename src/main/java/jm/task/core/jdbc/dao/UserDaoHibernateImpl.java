38f2ad04c5dcd768607599ec766edb6a19e862ec
dell <m>
2022-08-12 15:28:39 +0600

    JDBC and HIBERNATE

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -1,103 +1,133 @@
-package jm.task.core.jdbc.dao;
+//package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.util.Util;
 import org.hibernate.HibernateException;
 import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class UserDaoHibernateImpl implements UserDao {
-
-    Session session = Util.getSessionFactory().openSession();
-    public UserDaoHibernateImpl() {
-
-    }
-
-
-    @Override
-    public void createUsersTable() {
-        try{
-            session.beginTransaction();
-            session.createSQLQuery("CREATE TABLE IF NOT EXISTS users " +
-                    "(id SERIAL PRIMARY KEY ," +
-                    "name VARCHAR(50)," +
-                    "lastName VARCHAR(50)," +
-                    "age INTEGER)").executeUpdate();
-            session.getTransaction().commit();
-
-            System.out.println("Created successfully!!");
-        }catch (HibernateException h){
-            session.getTransaction().rollback();h.printStackTrace();
-        }
-    }
-
-    @Override
-    public void dropUsersTable() {
-    try{
-        session.beginTransaction();
-        session.createSQLQuery("drop table users").executeUpdate();
-        session.getTransaction().commit();
-
-        System.out.println("Table users dropped");
-    }catch (Exception h){
-        session.getTransaction().rollback(); h.printStackTrace();
-    }
-    }
-
-    @Override
-    public void saveUser(String name, String lastName, byte age) {
-    try{
-        session.beginTransaction();
-        session.persist(new User(name,lastName,age));
-        session.getTransaction().commit();
-
-        System.out.println("User with name " + name + " inserted to database");
-    }catch (Exception h){
-        session.getTransaction().rollback(); h.printStackTrace();
-    }
-    }
-
-    @Override
-    public void removeUserById(long id) {
-        try {
-            session.beginTransaction();
-            User user = session.get(User.class,id);
-            session.delete(user);
-            session.getTransaction().commit();
-
-            System.out.println("User with " + id + "id is deleted from database");
-        }catch (Exception h){
-            session.getTransaction().rollback(); h.printStackTrace();
-        }
-    }
-
-    @Override
-    public List<User> getAllUsers() {
-    List<User> users = new ArrayList<>();
-    try {
-        session.beginTransaction();
-        users = session.createQuery("select a from User a",User.class).getResultList();
-        session.getTransaction().commit();
-
-        System.out.println("Found " + users.size() + " users");
-    }catch (Exception h){
-        session.getTransaction().rollback();h.printStackTrace();
-    }
-        return users;
-    }
-
-    @Override
-    public void cleanUsersTable() {
-    try {
-        session.beginTransaction();
-        session.createSQLQuery("delete from users").executeUpdate();
-        session.getTransaction().commit();
-
-        System.out.println("Successfully deleted all data in users");
-    }catch (Exception h){
-        session.getTransaction().rollback(); h.printStackTrace();
-    }
-    }
-}
+//public class UserDaoHibernateImpl implements UserDao {
+//
+//    SessionFactory sessionFactory = Util.getSessionFactory();
+//    public UserDaoHibernateImpl() {
+//
+//    }
+//
+//
+//    @Override
+//    public void createUsersTable() {
+//        Transaction transaction = null;
+//        try(Session session = sessionFactory.openSession()){
+//            transaction = session.beginTransaction();
+//            session.createSQLQuery("CREATE TABLE IF NOT EXISTS users " +
+//                    "(id SERIAL PRIMARY KEY ," +
+//                    "name VARCHAR(50)," +
+//                    "lastName VARCHAR(50)," +
+//                    "age INTEGER)").executeUpdate();
+//            transaction.commit();
+//
+//            System.out.println("Created successfully!!");
+//        }catch (HibernateException h){
+//            h.printStackTrace();
+//            if (transaction!=null){
+//            transaction.rollback();
+//            }
+//        }
+//    }
+//
+//    @Override
+//    public void dropUsersTable() {
+//        Transaction transaction = null;
+//    try(Session session = sessionFactory.openSession()){
+//        transaction=session.beginTransaction();
+//        session.createSQLQuery("drop table users").executeUpdate();
+//        transaction.commit();
+//
+//        System.out.println("Table users dropped");
+//    }catch (Exception h){
+//        h.printStackTrace();
+//        if (transaction!=null){
+//            transaction.rollback();
+//        }
+//    }
+//    }
+//
+//    @Override
+//    public void saveUser(String name, String lastName, byte age) {
+//        Transaction transaction = null;
+//    try(Session session = sessionFactory.openSession()){
+//        transaction=session.beginTransaction();
+//        session.persist(new User(name,lastName,age));
+//        transaction.commit();
+//
+//        System.out.println("User with name " + name + " inserted to database");
+//    }catch (Exception h){
+//        h.printStackTrace();
+//        if (transaction!=null){
+//            transaction.rollback();
+//        }
+//    }
+//    }
+//
+//    @Override
+//    public void removeUserById(long id) {
+//        Transaction transaction = null;
+//        try (Session session = sessionFactory.openSession()){
+//            transaction=session.beginTransaction();
+//            User user = session.get(User.class,id);
+//            if (user!=null){
+//            session.delete(user);
+//                System.out.println("User with id: " + id + " deleted ");
+//            }else {
+//                System.out.println("There is no user with id " + id);
+//            }
+//            transaction.commit();
+//
+//            System.out.println("User with " + id + "id is deleted from database");
+//        }catch (Exception h){
+//            h.printStackTrace();
+//            if (transaction!=null){
+//                transaction.rollback();
+//            }
+//        }
+//    }
+//
+//    @Override
+//    public List<User> getAllUsers() {
+//        Transaction transaction = null;
+//    List<User> users = new ArrayList<>();
+//    try (Session session = sessionFactory.openSession()){
+//        transaction=session.beginTransaction();
+//        users = session.createQuery("select a from User a",User.class).getResultList();
+//
+//        transaction.commit();
+//
+//        System.out.println("Found " + users.size() + " users");
+//    }catch (Exception h){
+//        h.printStackTrace();
+//        transaction.rollback();
+//    }
+//        return users;
+//    }
+//
+//    @Override
+//    public void cleanUsersTable() {
+//        Transaction transaction = null;
+//    try (Session session = sessionFactory.openSession()){
+//        transaction=session.beginTransaction();
+//        session.createSQLQuery("delete from users").executeUpdate();
+//        transaction.commit();
+//
+//        System.out.println("Successfully deleted all data in users");
+//    }catch (Exception h){
+//        h.printStackTrace();
+//        if (transaction != null) {
+//            transaction.rollback();
+//        }
+//    }
+//    }
+//}
--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -13,95 +13,129 @@ public class UserDaoJDBCImpl implements UserDao {
     public UserDaoJDBCImpl() {
 
     }
-
+@Override
     public void createUsersTable() throws SQLException {
 
-        try (Connection connection = Util.connection();
-              Statement statement = connection.createStatement()){
-              statement.executeUpdate(" CREATE TABLE users (id serial primary key , " +
-                      " name varchar(20) not null, " +
-                      " lastName varchar(30) not null, " +
-                      " age int)");
-                connection.commit();
+        try (Connection connection = Util.connection()) {
 
+            connection.setAutoCommit(false);
+            try (Statement statement = connection.createStatement()) {
+                statement.executeUpdate(" CREATE TABLE users (id serial primary key , " +
+                        " name varchar(20) not null, " +
+                        " lastName varchar(30) not null, " +
+                        " age int)");
+            } catch (SQLException e) {
+                connection.rollback();
+                e.printStackTrace();
+            }
+            connection.commit();
             System.out.println("Table Users created!!");
+        } catch (SQLException e) {
+            e.printStackTrace();
+
+        }
+    }
+@Override
+    public void dropUsersTable() throws SQLException {
+    try (Connection connection = Util.connection()) {
+        connection.setAutoCommit(false);
+        try (Statement statement = connection.createStatement()) {
+            statement.executeUpdate("drop table users");
+            System.out.println("Table Users dropped!!");
+            connection.commit();
+        } catch (SQLException e) {
+            connection.rollback();
+            e.printStackTrace();
+        }
+    } catch (SQLException e) {
+        e.printStackTrace();
+
+    }
+}
+
+
+    @Override
+    public void saveUser(String name, String lastName, byte age) throws SQLException {
+        String sql = "insert into users (name,lastName,age) values (?,?,?)";
+        try (Connection connection = Util.connection()){
+            connection.setAutoCommit(false);
+            try (PreparedStatement preparedStatement = connection.prepareStatement(sql)){
+                preparedStatement.setString(1, name);
+                preparedStatement.setString(2, lastName);
+                preparedStatement.setByte(3, age);
+                preparedStatement.executeUpdate();
+                connection.commit();
+                System.out.println("User with name " + name + " inserted to database");
+
+            } catch (SQLException e) {
+                connection.rollback();
+                e.printStackTrace();
+            }
+
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void removeUserById(long id) throws SQLException {
+        try (Connection connection = Util.connection()){
+            connection.setAutoCommit(false);
+            try (PreparedStatement preparedStatement = connection.prepareStatement("delete from users where id=?")) {
+                preparedStatement.setLong(1, id);
+                preparedStatement.executeUpdate();
+                connection.commit();
+                System.out.println("User with " + id + "id is deleted from database");
+            }catch (SQLException e) {
+                connection.rollback();
+                e.printStackTrace();
+            }
         } catch (SQLException e) {
             e.printStackTrace();
         }
     }
 
-    public void dropUsersTable() throws SQLException {
-        try (Connection connection = Util.connection();
-        Statement statement = connection.createStatement()) {
-            statement.executeUpdate("drop table users");
-            System.out.println("Table Users dropped!!");
-            connection.commit();
-
-        }catch (SQLException e){
-            e.printStackTrace();
-        }
-
-    }
-
-    public void saveUser(String name, String lastName, byte age) throws SQLException {
-        String sql = "insert into users (name,lastName,age) values (?,?,?)";
-        try (Connection connection = Util.connection();
-             PreparedStatement preparedStatement = connection.prepareStatement(sql)){
-             preparedStatement.setString(1, name);
-             preparedStatement.setString(2, lastName);
-             preparedStatement.setByte(3, age);
-             preparedStatement.executeUpdate();
-             connection.commit();
-
-
-            System.out.println("User with name " + name + " inserted to database");
-
-        }catch (SQLException e){
-            e.printStackTrace();
-        };
-
-    }
-
-    public void removeUserById(long id) throws SQLException {
-    try (Connection connection = Util.connection();
-    PreparedStatement preparedStatement = connection.prepareStatement("delete from users where id=?")){
-    preparedStatement.setLong(1, id);
-    preparedStatement.executeUpdate();
-    connection.commit();
-        System.out.println("User with " + id + "id is deleted from database");
-
-    }catch (SQLException e){
-        e.printStackTrace();
-    }
-    }
-
+    @Override
     public List<User> getAllUsers() throws SQLException {
         List<User> users = new ArrayList<>();
-        try (Connection connection = Util.connection();
-             Statement statement = connection.createStatement();
-             ResultSet resultSet = statement.executeQuery("select * from users")) {
-            while (resultSet.next()) {
-                User user = new User(
-                        resultSet.getString("name"),
-                        resultSet.getString("lastName"),
-                        resultSet.getByte("age"));
-                users.add(user);
-            }connection.commit();
-            System.out.println(users.size() + " users founded: " + users);
-        } catch (SQLException throwables) {
-            System.out.println(throwables.getMessage());
+        try (Connection connection = Util.connection()){
+            try (Statement statement = connection.createStatement()) {
+                ResultSet resultSet = statement.executeQuery("select * from users");
+                while (resultSet.next()) {
+                    User user = new User(
+                            resultSet.getString("name"),
+                            resultSet.getString("lastName"),
+                            resultSet.getByte("age"));
+                    users.add(user);
+                }
+                connection.commit();
+                System.out.println(users.size() + " users founded: " + users);
+            }catch (SQLException throwable) {
+                connection.rollback();
+                System.out.println(throwable.getMessage());
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
         return users;
     }
 
+    @Override
     public void cleanUsersTable() throws SQLException {
-        try (Connection connection = Util.connection();
-             Statement statement = connection.createStatement()) {
-            statement.executeUpdate("delete from users;");
-            connection.commit();
-            System.out.println("DELETED ALL USERS");
-        } catch (SQLException throwables) {
-             System.out.println(throwables.getMessage());
+        try (Connection connection = Util.connection()){
+            connection.setAutoCommit(false);
+            try(Statement statement = connection.createStatement()){
+                statement.executeUpdate("delete from users;");
+                connection.commit();
+                System.out.println("DELETED ALL USERS");
+            }catch (SQLException throwable) {
+                connection.rollback();
+                System.out.println(throwable.getMessage());
+            }
+        }catch (SQLException e) {
+            e.printStackTrace();
         }
     }
 }
+
